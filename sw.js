3ff3cd5db42943fdac1e69aa0a91b0ae5dfcd064
Lila <himeur.lila13@gmail.com>
2026-05-13 10:22:20 +0200

    sw: cache v69 après revert icônes nav

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sakinapp-v67';
+const CACHE = 'sakinapp-v69';
 const ASSETS = ['/', '/index.html', '/style.css', '/data.js', '/app.js', '/manifest.json', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
